65e11a6990f9aa0cf586d54a2c5c6ad589051f0e
guiac <gui.acassemiro@gmail.com>
2023-01-21 09:10:00 -0300

    :construction: removing unecessary files

--- src/main/routes/account.routes.ts
+++ src/main/routes/account.routes.ts
@@ -1,11 +1,9 @@
 import { adaptRoute } from '@/main/adapters'
-import { makeSignUpController, makeUpdateAccountControler, makeAuthenticationController } from '@/main/factories'
+import { } from '@/main/factories'
 
 import { Router } from 'express'
 import { auth } from '@/main/middlewares'
 
 export default (router: Router): void => {
-    router.post('/signup', adaptRoute(makeSignUpController()))
-    router.post('/login', adaptRoute(makeAuthenticationController()))
-    router.put('/account/update/:identification', auth, adaptRoute(makeUpdateAccountControler()))
+    // router.post('/signup', adaptRoute(makeSignUpController()))
 }
