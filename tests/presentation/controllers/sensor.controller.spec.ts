317166b4ad3e8335273afaaf99e3183661379f17
guiac <gui.acassemiro@gmail.com>
2023-01-21 10:08:11 -0300

    :test_tube: Should return 500 if Validation throws

--- tests/presentation/controllers/sensor.controller.spec.ts
+++ tests/presentation/controllers/sensor.controller.spec.ts
@@ -1,10 +1,15 @@
 import { ValidationSpy } from '../mocks'
 import { SensorController } from '@/presentation/controllers'
 
+const throwError = (): never => {
+    throw new Error()
+}
+
 type SutTypes = {
     validationSpy: ValidationSpy
     sut: SensorController
 }
+
 const mockSut = (): SutTypes => {
     const validationSpy = new ValidationSpy()
     const sut = new SensorController(validationSpy)
@@ -37,6 +42,14 @@ describe('SensorController', () => {
     test('Should return 400 if validation fails', async () => {
         const { sut } = mockSut()
         const request = mockRequest()
+        const httpResponse = await sut.handle(request)
+        expect(httpResponse.statusCode).toBe(400)
+    })
+
+    test('Should return 500 if Validation throws', async () => {
+        const { sut, validationSpy } = mockSut()
+        const request = mockRequest()
+        jest.spyOn(validationSpy, 'validate').mockImplementationOnce(throwError)
         const httpResponse = await sut.handle(request)
         expect(httpResponse.statusCode).toBe(400)
     })
