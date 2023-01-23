b92d87f3038fdf19ac16356a2a1f4b720c0cdb02
guiac <gui.acassemiro@gmail.com>
2023-01-22 21:42:09 -0300

    :sparkles: sensor route

--- src/main/routes/sensor.routes.ts
+++ src/main/routes/sensor.routes.ts
@@ -1,9 +1,9 @@
 import { adaptRoute } from '@/main/adapters'
-import { } from '@/main/factories'
+import { makeCreateSensorController } from '@/main/factories'
 
 import { Router } from 'express'
 import { auth } from '@/main/middlewares'
 
 export default (router: Router): void => {
-    // router.post('/signup', adaptRoute(makeSignUpController()))
+    router.post('/sensor', auth, adaptRoute(makeCreateSensorController()))
 }
