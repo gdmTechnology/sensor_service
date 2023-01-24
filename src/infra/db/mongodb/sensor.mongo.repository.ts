30a1723260145df8851db9d698c6cb3322523a0d
guiac <gui.acassemiro@gmail.com>
2023-01-23 23:08:46 -0300

    :sparkles: SensorMongoRepository update()

--- src/infra/db/mongodb/sensor.mongo.repository.ts
+++ src/infra/db/mongodb/sensor.mongo.repository.ts
@@ -1,12 +1,25 @@
-import { SaveSensorRepository } from '@/data/protocols'
+import { SaveSensorRepository, UpdateSensorRepository } from '@/data/protocols'
 import { SensorModel } from './models'
 
-export class SensorMongoRepository implements SaveSensorRepository {
+export class SensorMongoRepository implements SaveSensorRepository, UpdateSensorRepository {
     async save(data: SaveSensorRepository.Params): Promise<SaveSensorRepository.Result> {
-        const model = new SensorModel(data)
-        const result = await model.save()
+        const result = await SensorModel.create(data)
         if (result.accountId) return result
         return null
+    }
+
+    async update(data: UpdateSensorRepository.Params): Promise<UpdateSensorRepository.Result> {
+        const { sensorIdentification, sensorName, sensorEquipment, sensorMeasureType, sensorCurrentValue, sensorTimeStamp } = data
+        const filter = { sensorIdentification }
+        const update: any = { sensorName, sensorEquipment, sensorMeasureType, sensorCurrentValue, sensorTimeStamp }
+        sensorName === undefined && delete update.sensorName
+        sensorEquipment === undefined && delete update.sensorEquipment
+        sensorMeasureType === undefined && delete update.sensorMeasureType
+        sensorCurrentValue === undefined && delete update.sensorCurrentValue
+        sensorTimeStamp === undefined && delete update.sensorTimeStamp
+        const option = { new: true }
+        const result = SensorModel.findOneAndUpdate(filter, update, option).lean()
+        return result
     }
 }
 
