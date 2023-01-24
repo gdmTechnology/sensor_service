2d603840c9ff0fb482a1a1e4b5073437d394d5fe
guiac <gui.acassemiro@gmail.com>
2023-01-23 23:16:10 -0300

    :sparkles: makeUpdateSensorValidation

---
+++ src/main/factories/controllers/update.sensor.validation.factory.ts
@@ -0,0 +1,10 @@
+import { RequiredFieldValidation, ValidationComposite } from '@/validation/validators'
+import { Validation } from '@/presentation/protocols'
+
+export const makeUpdateSensorValidation = (): ValidationComposite => {
+    const validations: Validation[] = []
+    for (const field of ['sensorIdentification']) {
+        validations.push(new RequiredFieldValidation(field))
+    }
+    return new ValidationComposite(validations)
+}
