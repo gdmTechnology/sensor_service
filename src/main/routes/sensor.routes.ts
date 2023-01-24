d0a83b34af4610de4df9f111aa3a3f54ae56edc2
guiac <gui.acassemiro@gmail.com>
2023-01-23 23:16:58 -0300

    :sparkles: update sensor route

--- src/main/routes/sensor.routes.ts
+++ src/main/routes/sensor.routes.ts
@@ -1,9 +1,10 @@
 import { adaptRoute } from '@/main/adapters'
-import { makeCreateSensorController } from '@/main/factories'
+import { makeCreateSensorController, makeUpdateSensorController } from '@/main/factories'
 
 import { Router } from 'express'
 import { auth } from '@/main/middlewares'
 
 export default (router: Router): void => {
     router.post('/sensor', auth, adaptRoute(makeCreateSensorController()))
+    router.put('/sensor', auth, adaptRoute(makeUpdateSensorController()))
 }
