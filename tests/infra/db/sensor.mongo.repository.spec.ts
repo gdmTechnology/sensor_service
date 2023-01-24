3a9c9524c76415cc80de05fd92106aef23d40550
guiac <gui.acassemiro@gmail.com>
2023-01-23 23:08:31 -0300

    :test_tube: SensorMongoRepository update()

--- tests/infra/db/sensor.mongo.repository.spec.ts
+++ tests/infra/db/sensor.mongo.repository.spec.ts
@@ -2,6 +2,9 @@
 import { SensorMongoRepository } from '@/infra/db/mongodb'
 import { MongoTestDbHelper } from './db.handler'
 
+const throwError = (): never => {
+    throw new Error()
+}
 const makeSut = (): SensorMongoRepository => {
     return new SensorMongoRepository()
 }
@@ -22,17 +25,46 @@ describe('SensorMongoRepository', () => {
     afterEach(async () => await MongoTestDbHelper.clearDatabase())
     afterAll(async () => await MongoTestDbHelper.disconnect())
 
-    test('Should return an sensor on success', async () => {
-        const sut = makeSut()
-        const params = createSensorParams()
-        const sensor = await sut.save(params)
-        expect(sensor).toBeDefined()
+    describe('save()', () => {
+        test('Should return an sensor on success', async () => {
+            const sut = makeSut()
+            const params = createSensorParams()
+            const sensor = await sut.save(params)
+            expect(sensor).toBeDefined()
+        })
+
+        test('Should throw if SensorMongoRepository throws', async () => {
+            const sut = makeSut()
+            const params = createSensorParams()
+            const promise = sut.save({ ...params, accountId: '' })
+            await expect(promise).rejects.toThrow()
+        })
     })
 
-    test('Should throw if SensorMongoRepository throws', async () => {
-        const sut = makeSut()
-        const params = createSensorParams()
-        const promise = sut.save({ ...params, accountId: '' })
-        await expect(promise).rejects.toThrow()
+    describe('update()', () => {
+        test('Should return a sensor updated on success', async () => {
+            const sut = makeSut()
+            const params = createSensorParams()
+            const sensor = await sut.save(params)
+            const { sensorIdentification } = sensor
+            const sensorUpdated = await sut.update({ sensorIdentification, sensorName: 'sensorNameUpdated' })
+            console.log('sensorUpdated', sensorUpdated)
+            expect(sensorUpdated.sensorName).toBe('sensorNameUpdated')
+        })
+
+        test('Should return null if update() fails', async () => {
+            const sut = makeSut()
+            const params = createSensorParams()
+            const result = await sut.update({ ...params, sensorIdentification: '' })
+            expect(result).toBeNull()
+        })
+
+        test('Should throw if update() throws', async () => {
+            const sut = makeSut()
+            jest.spyOn(sut, 'update').mockReturnValueOnce(new Promise((resolve, reject) => reject(throwError)))
+            const params = createSensorParams()
+            const promise = sut.update(params)
+            await expect(promise).rejects.toThrow()
+        })
     })
 })
