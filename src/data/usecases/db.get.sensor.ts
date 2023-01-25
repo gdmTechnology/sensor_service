feb1898d1179bc215df9594d0408f9a4bc0b252e
guiac <gui.acassemiro@gmail.com>
2023-01-24 21:40:27 -0300

    :sparkles: DbGetSensor

---
+++ src/data/usecases/db.get.sensor.ts
@@ -0,0 +1,22 @@
+import { GetSensor } from '@/domain/usecases'
+import { GetSensorRepository } from '@/data/protocols'
+import { ApplicationError, Either, error, success } from '@/domain/protocols'
+import { Constants } from '@/helper'
+
+export class DbGetSensor implements GetSensor {
+    constructor(
+        private readonly getSensorRepository: GetSensorRepository
+    ) { }
+
+    async handle(sensorIdentification: string): Promise<Either<ApplicationError, GetSensor.Result>> {
+        const sensor = await this.getSensorRepository.get(sensorIdentification)
+        if (!sensor) {
+            const appError = new ApplicationError(
+                Constants.NotFoundSensor.error,
+                Constants.NotFoundSensor.message
+            )
+            return error(appError)
+        }
+        return success(sensor)
+    }
+}
