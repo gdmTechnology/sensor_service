cc11bdc8447e6049d26e78d6f482b92ecbaabf61
guiac <gui.acassemiro@gmail.com>
2023-01-26 22:44:21 -0300

    :sparkles: get sensor route

--- src/main/routes/sensor.routes.ts
+++ src/main/routes/sensor.routes.ts
@@ -1,5 +1,10 @@
 import { adaptRoute } from '@/main/adapters'
-import { makeCreateSensorController, makeUpdateSensorController, makeGetSensorsListController } from '@/main/factories'
+import {
+    makeCreateSensorController,
+    makeUpdateSensorController,
+    makeGetSensorsListController,
+    makeGetSensorController
+} from '@/main/factories'
 
 import { Router } from 'express'
 import { auth } from '@/main/middlewares'
@@ -8,4 +13,5 @@ export default (router: Router): void => {
     router.post('/sensor', auth, adaptRoute(makeCreateSensorController()))
     router.put('/sensor/:sensorIdentification', auth, adaptRoute(makeUpdateSensorController()))
     router.get('/sensor', auth, adaptRoute(makeGetSensorsListController()))
+    router.get('/sensor/:sensorIdentification', auth, adaptRoute(makeGetSensorController()))
 }
